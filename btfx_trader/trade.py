53af53c244b4612cf512395058c3c03773e0f7bc
Djordje <djopep@gmail.com>
2017-11-28 00:19:01 +0100

    Trader fully functional

--- btfx_trader/trade.py
+++ btfx_trader/trade.py
@@ -4,25 +4,22 @@ import time
 from threading import Thread
 
 from btfxwss import BtfxWss
+from bitex import Bitfinex
 
 log = logging.getLogger(__name__)
 
 
 class Trader:
-    def __init__(self, symbol, auth, total_balance_portion=0.2):
+    def __init__(self, symbol, auth):
         self.symbol = symbol
         self.api = BtfxWss(*auth, log_level='CRITICAL')
-        self.balance_portion = total_balance_portion
-        self.balance, self.coin = self.update_wallet()
+        self.rest = Bitfinex(*auth)
+        self.balance, self.coin = self.wallet('usd'), self.wallet(self.symbol)
         self.price = 0.
         self.connected = False
         self.running = True
         self.updater = Thread(target=self.update_price)
         self.updater.start()
-
-    def update_wallet(self):
-        # todo get current total balance * self.balance_portion
-        return 0, 0
 
     def update_price(self):
         self.api.start()
@@ -42,25 +39,42 @@ class Trader:
         while self.running:
             self.price = tickers.get()[0][0][6]
 
+    def wallet(self, symbol):
+        resp = self.rest.private_query('balances').json()
+        for i in resp:
+            if i['type'] == 'exchange' and i['currency'] == symbol:
+                return eval(i['available'])
+
+    def _order(self, symbol, amount, side):
+        q = {'symbol': symbol, 'amount': amount, 'side': side,
+             'type': 'exchange market', 'price': '100000.0'}
+        return self.rest.private_query('order/new', params=q)
+
     def buy(self, percentage):
+        self.balance, self.coin = self.wallet('usd'), self.wallet(self.symbol)
         if self.balance > 1:
-            n = round(self.balance * percentage / self.price, 8)
-            # todo execute trade
-            # todo make sure price and executed price are the same
-            self.balance -= self.price * n
-            self.coin += n
-            log.warning('Buying %s at %-.2f for %.1f!', self.symbol, self.price, n * self.price)
+            n = round(self.balance * percentage / self.price * 0.9999, 8)
+            resp = self._order(self.symbol, '%.5f' % n, 'buy')
+            if resp.status_code != 200:
+                log.error('Buy order returned error %d, %s', resp.status_code, resp.text)
+            else:
+                self.balance -= self.price * n
+                self.coin += n
+                log.warning('Bought %s at %-.2f for %.1f!', self.symbol, self.price, n * self.price)
         else:
             log.error('Tried to buy when balance is less than $1, %f', self.balance)
 
     def sell(self, percentage):
+        self.balance, self.coin = self.wallet('usd'), self.wallet(self.symbol)
         if self.coin > 0:
-            n = round(self.coin * percentage, 8)
-            # todo execute trade
-            # todo make sure price and executed price are the same
-            self.balance += self.price * n
-            self.coin -= n
-            log.warning('Selling %s at %-.2f for %.1f!', self.symbol, self.price, n * self.price)
+            n = round(self.coin * percentage, 8) * 0.9999
+            resp = self._order(self.symbol, '%.5f' % n, 'sell')
+            if resp.status_code != 200:
+                log.error('Sell order returned error %d, %s', resp.status_code, resp.text)
+            else:
+                self.balance += self.price * n
+                self.coin -= n
+                log.warning('Sold %s at %-.2f for %.1f!', self.symbol, self.price, n * self.price)
         else:
             log.error('Tried to buy when coin is 0, %f', self.coin)
 
