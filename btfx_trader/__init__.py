f487effd541cceb3c726941d961ebd22bad98dd6
sentrip <djordje.m.pepic@gmail.com>
2018-05-27 20:25:52 +0200

    Bump version: 0.1.0 → 0.1.1

--- btfx_trader/__init__.py
+++ btfx_trader/__init__.py
@@ -3,7 +3,7 @@ from .private import Trader
 
 __author__ = """Djordje Pepic"""
 __email__ = 'djordje.m.pepic@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = [
     'get_symbols', 'get_symbols_as_updated',
